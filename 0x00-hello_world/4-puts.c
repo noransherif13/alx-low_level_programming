69bc7fdf9db9189fed167f0ab743ebda9c66c284
Noran Sherif <Noranosherif958@gmail.com>
2023-06-23 08:29:58 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 /* This is the main function that executes the program */
 int main(void)
 {
+	//function implementation goes here
 const char *messege = "\"Programming is like building a multilingual puzzle";
 puts(messege);
 return (0);
