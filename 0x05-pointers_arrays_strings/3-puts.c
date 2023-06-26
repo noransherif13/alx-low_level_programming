7c33e9bac4dd5846aec2d13031b3c0c23ea5a716
Noran Sherif <>
2023-06-26 16:31:40 -0700

    print str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: Input string parameter
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		printf(*str + 0);
+		str++;
+	}
+	_putchar("\n");
+}
