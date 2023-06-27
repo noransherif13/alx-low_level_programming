946e57743cd7c645505d54c0648f37a7989e1cb5
Noran Sherif <>
2023-06-27 04:17:00 -0700

    print every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character in the string
+ *
+ * @str: input string parameter
+ *
+ * Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
