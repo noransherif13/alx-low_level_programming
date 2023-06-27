f9fbd435afc76b05992a26cc08d8b00b6645d7ad
Noran Sherif <>
2023-06-27 04:45:41 -0700

    another solution

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (i = 0; str[i] != '\0'; ++i)
+	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		_putchar(str[i]);
+		i += 2;
 	}
 
 	_putchar('\n');
