8abe7e8d5bba540c4997183c098337d2771a021a
Noran Sherif <>
2023-06-29 13:46:48 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - functions that concatenas
+ * n characters of src to dest
+ *
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ *
+ * Return: pointer to string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; j < n; j++)
+		dest[i++] = src[j];
+	return (dest);
+}
