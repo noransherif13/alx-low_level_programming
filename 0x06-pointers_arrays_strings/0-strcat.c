5b9fa16226148344582da3433983f2762d8d4a7d
Noran Sherif <>
2023-06-29 12:41:51 -0700

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#define "main.h"
+
+/**
+ * _strcat : - Function that concatenats two string
+ *
+ * @dest: pointer to char parameter 1
+ * @src: pointer to char parameter 2
+ *
+ * Return: pointer to char array
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
