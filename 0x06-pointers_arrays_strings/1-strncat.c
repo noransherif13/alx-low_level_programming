e2583609642cde9bc34e3cc9839d3b0099131590
Noran Sherif <>
2023-06-29 13:50:51 -0700

    strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && str[i] != '\0'; j++)
 		dest[i++] = src[j];
 	dest[i] = '\0';
 	return (dest);
