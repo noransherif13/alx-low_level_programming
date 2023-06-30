3f48461bf831be92e05572d99623911cc9731115
Noran Sherif <>
2023-06-29 17:26:26 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,18 +6,18 @@
  * @a: parameter 1
  * @n: parameter 2
  *
- * Return: always 0 (success)
+ * Return: void
 */
 
 void reverse_array(int *a, int n)
 {
-	int i, j, t;
+	int i, t;
 
-	for (i = 0, j = n - 1; i < j, i++, j++;)
+	for (i = 0; i < n--, i++)
 	{
 		t = a[i];
-		a[i] = a[j];
-		a[j] = t;
+		a[i] = a[n];
+		a[n] = t;
 	}
-	return (0);
+	
 }
