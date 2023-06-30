d7fe840162032bcb9d66acccc1123b60924e0dd0
Noran Sherif <>
2023-06-29 17:00:49 -0700

    string comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	int equal = 0;
+
+	while (*s1)
 	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
 		s1++;
 		s2++;
 	}
-	return *(unsigned char*)s1 - *(unsigned char*)s2;
+	return (equal);
+
 
 }
