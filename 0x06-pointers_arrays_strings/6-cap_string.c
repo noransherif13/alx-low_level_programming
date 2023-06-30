88445523d77834bfa8fa36df3ec1ef988efc10d9
Noran Sherif <>
2023-06-30 12:37:21 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' || str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == ' ' ||
 				str[i - 1] == '\n' ||
@@ -33,5 +33,6 @@ char *cap_string(char *str)
 			str[i] -= 32;
 		i++;
 	}
+
 	return (str);
 }
