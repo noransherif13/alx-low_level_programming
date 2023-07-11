783d6df8b06363c0f34c6e0cf0c9fb7f068733b4
Noran Sherif <>
2023-07-11 13:00:03 -0700

    task 2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,12 @@
  * main -print number of arguments passed
  * @argc:nmuber of commandline arguments
  * @argv:pointer to an array of command line arguments
- * Rturn:0-sucess, non-zero-fail
+ * Return:0-sucess, non-zero-fail
 */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
+(void) argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
