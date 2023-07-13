0e680f1495cde225b4f8bcad0c04acc3351224a1
Noran Sherif <>
2023-07-13 13:14:24 -0700

    numberz task

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * function that prints from 0-9
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
