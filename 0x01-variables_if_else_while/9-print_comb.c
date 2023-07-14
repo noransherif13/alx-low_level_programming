27513c7750f57e4985281799aa503a912dd0514e
Noran Sherif <>
2023-07-14 03:21:38 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints all possible
+ * combinations of single numbers
+ *
+ * Return: always zero (success)
+*/
+
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 9)
+		putchar(i + '0');
+		else
+		{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		}
+		i++;
+
+	}
+	return (0);
+}
