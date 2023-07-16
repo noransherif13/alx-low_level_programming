2fc4906c30d387802d416e590134c1aa9c2b9704
Noran Sherif <>
2023-07-16 10:57:52 -0700

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,9 +32,9 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
@@ -48,8 +48,9 @@ char *str_concat(char *s1, char *s2)
 			m[i] = s1[i];
 		else
 			m[i] = s2[i - size1];
-		m[i] = '\0';
+
 	}
+	m[i] = '\0';
 	return (m);
 
 }
