1cb378eab5b3ad6a106ff6c320479faaeeeee76f
Noran Sherif <>
2023-07-16 02:36:50 -0700

    initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *create_array - function that creates an array of chars
+ *
+ * @size: parameter 1
+ * @c: parameter 2
+ *
+ * Return: Pointer to an array of char or NULL
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+
+	while (size--)
+		n[size] = c;
+}
