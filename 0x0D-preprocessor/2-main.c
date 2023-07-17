a15220291e49a67e1a2348632a260271a4e40818
Noran Sherif <>
2023-07-17 04:43:03 -0700

    __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints its file path
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
