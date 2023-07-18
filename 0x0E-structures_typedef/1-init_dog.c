a4eb5a15b9dcbb53caa2331fc063b5b66f005f9d
Noran Sherif <>
2023-07-18 07:22:01 -0700

    dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog -function
+ *
+ * @d:d
+ * @name:name
+ * @age:age
+ * @owner:owner
+ *
+ * Return:void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
