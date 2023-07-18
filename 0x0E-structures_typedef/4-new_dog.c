bda8246e8cd6357fad22709f3925899675ef23ce
Noran Sherif <>
2023-07-18 09:56:27 -0700

    malloc struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,80 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * strlen -returns length
+ *
+ * @str:input1
+ *
+ * Return: string length
+*/
+
+int strlen(const char *str)
+{
+	int length = 0;
+
+	while (str)
+		length++;
+	else
+		return (length);
+}
+
+/**
+ * strcopy -function
+ * @dest: parameter1
+ * @src:parameter2
+ * Return: ptr
+*/
+char *strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+
+}
+
+/**
+ * new_dog -function
+ * @name:parameter1
+ * @owner:parameter2
+ * @age:parameter3
+ * Return:dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * strlen(name) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner, owner);
+	return (dog);
+
+}
