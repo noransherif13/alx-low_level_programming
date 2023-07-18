7a9d108d911a6978847f6770098cbd61555c8713
Noran Sherif <>
2023-07-18 06:47:01 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -a dog struct
+ *
+ * @name: char*
+ * @age :float
+ * @owner:char
+ *
+ * Description: struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
