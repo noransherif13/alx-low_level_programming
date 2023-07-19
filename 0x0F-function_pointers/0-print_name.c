161e231fa78dabbad06314a06d995bbc63e377fd
Noran Sherif <>
2023-07-19 09:52:37 -0700

    call a function to a pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -function
+ * @name:char
+ * @f:pointer to a function
+ * Return:void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
